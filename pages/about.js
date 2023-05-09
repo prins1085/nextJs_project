c1452f13a33583d4d7cf185ad833e3b558167396
Prins Harkhani <harkhaniprins2001@gmail.com>
2023-05-09 11:20:38 +0530

    Modified About page

--- pages/about.js
+++ pages/about.js
@@ -1,40 +1,48 @@
-import React from "react";
-import Navbar from "./Navbar";
-import Head from "next/head";
+// import React from "react";
+// import Navbar from "./Navbar";
 
-export async function getStaticProps() {
-  console.log("Revalidate page");
-  const res = await fetch("http://localhost:4000/products");
-  const posts = await res.json();
+// export async function getStaticProps() {
+//   console.log("Revalidate page");
+//   const res = await fetch("http://localhost:4000/products");
+//   const posts = await res.json();
 
-  return {
-    props: {
-      posts,
-    },
-    revalidate:10,
-  };
+//   return {
+//     props: {
+//       posts,
+//     },
+//     revalidate:10,
+//   };
+// }
+
+// const about = ({ posts }) => {
+//   return (
+//     <>
+//     <Navbar/>
+  
+//     <div style={{ padding: 30 }}>
+//       <div>
+//         {posts.map(post =>
+//           <div
+//             key={post.id}
+//             style={{ padding: 20, borderBottom: '1px solid #ccc' }}>
+//             <h2>{post.title}</h2>
+//             <p>{post.content}</p>
+//           </div>)}
+//       </div>
+//     </div>
+//     </>
+//   );
+// };
+
+// export default about;
+
+
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>about</div>
+  )
 }
 
-const about = ({ posts }) => {
-  return (
-    <>
-    <Navbar/>
-  
-    <div style={{ padding: 30 }}>
-      <div>
-        {posts.map(post =>
-          <div
-            key={post.id}
-            style={{ padding: 20, borderBottom: '1px solid #ccc' }}>
-            <h2>{post.title}</h2>
-            <p>{post.content}</p>
-          </div>)}
-      </div>
-    </div>
-    </>
-  );
-};
-
-export default about;
-
-
+export default about
